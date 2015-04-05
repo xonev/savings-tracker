de500a9ab88433931e7d482af09330230d9db630
xonev <steven.aj.oxley@gmail.com>
2015-04-04 20:55:53 -0700

    Make goal edit component.

--- src/cljs/savings_tracker/core.cljs
+++ src/cljs/savings_tracker/core.cljs
@@ -1,6 +1,8 @@
 (ns savings-tracker.core
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]])
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [cljs.core.async :refer [put! <! chan]]))
 
 (def app-state (atom {:balance 1000
                       :goals [{:name "Vacation"
@@ -14,6 +16,10 @@
                                :amount 500
                                :saved 259.45}]}))
 
+(defn add-goal
+  [goals]
+  (om/transact! goals #(conj % {:name "Cheesey Poofs" :start "2015-04-01T00:00:00Z" :end "2015-04-30T00:00:00Z" :amount 1000 :saved 0})))
+
 (defn goal-view
   [goal owner]
   (reify
@@ -21,12 +27,33 @@
     (render [_]
       (dom/li nil (str (:name goal) " " (:saved goal) "/" (:amount goal) " " (:end goal))))))
 
-(defn goal-views
+(defn input-view
+  [goal owner]
+  (reify
+    om/IRenderState
+    (render-state [owner {:keys [label]}]
+      (dom/label nil
+        label
+        (dom/input #js {:type "text"})))))
+
+(defn goal-edit-view
+  [goal owner]
+  (reify
+    om/IRender
+    (render [_]
+      (apply dom/div #js {:className "row"}
+        (for [label ["Name" "Start" "End" "Amount"]]
+          (om/build input-view goal {:init-state {:label label}}))))))
+
+(defn goals-view
   [goals owner]
   (reify
     om/IRender
     (render [_]
-      (apply dom/ul nil (om/build-all goal-view goals)))))
+      (apply dom/ul nil (dom/li nil
+                          (om/build goal-edit-view {})
+                          (dom/a #js {:onClick #(add-goal goals) :className "button tiny"} "Add Goal"))
+                        (om/build-all goal-view goals)))))
 
 (defn main []
   (om/root
@@ -37,6 +64,6 @@
           (dom/div nil
             (dom/h1 nil "Savings Tracker")
             (dom/h2 nil (str "Balance: " (:balance app)))
-            (om/build goal-views (:goals app))))))
+            (om/build goals-view (:goals app))))))
     app-state
     {:target (. js/document (getElementById "app"))}))
