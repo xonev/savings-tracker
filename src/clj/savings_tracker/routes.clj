e3c57e9d194a65c72210b8b7a9ee59440875edb6
Steven Oxley <steven.aj.oxley@gmail.com>
2016-03-14 18:38:53 -0700

    Update routing to use bidi.

--- src/clj/savings_tracker/routes.clj
+++ src/clj/savings_tracker/routes.clj
@@ -2,13 +2,19 @@
   (:require [clojure.java.io :as io]
             [savings-tracker.dev :refer [is-dev? inject-devmode-html]]
             [net.cgrand.enlive-html :refer [deftemplate]]
+            [bidi.ring :refer [make-handler ->Resources]]
             [compojure.core :refer [GET defroutes]]
             [compojure.route :refer [resources]]))
 
 (deftemplate page (io/resource "index.html") []
   [:body] (if is-dev? inject-devmode-html identity))
 
-(defroutes routes
-  (resources "/")
-  (resources "/react" {:root "react"})
-  (GET "/*" req (page)))
+(def routes
+  (make-handler
+    ["/" [["react" (->Resources {:prefix "react"})]
+          ["js" (->Resources {:prefix "public/js"})]
+          ["css" (->Resources {:prefix "public/css"})]
+          ["vendor" (->Resources {:prefix "public/vendor"})]
+          [true (fn [request] {:status 200
+                               :headers {"Content-Type" "text/html"}
+                               :body (apply str (page))})]]]))
